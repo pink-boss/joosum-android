5cfad423b0ebd8b9b8501c067580fbc259bb17fc
최현정 <hjchoi.dev@gmail.com>
2023-12-03 21:12:41 +0900

    Init : Create project

---
+++ app/src/main/kotlin/com/pinkboss/joosum/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pinkboss.joosum.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pinkboss.joosum.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
