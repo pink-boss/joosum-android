ecc6bf43381fb9bd64fc98c6fa121da93c026489
최현정 <hjchoi.dev@gmail.com>
2023-12-10 19:12:27 +0900

    Feat : Create LoginActivity UI

--- app/src/main/kotlin/com/pinkboss/joosum/presentation/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/pinkboss/joosum/presentation/splash/SplashActivity.kt
@@ -1,16 +1,12 @@
 package com.pinkboss.joosum.presentation.splash
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
-import com.pinkboss.joosum.presentation.MainActivity
+import com.pinkboss.joosum.presentation.login.LoginActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -34,7 +30,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun moveToMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
