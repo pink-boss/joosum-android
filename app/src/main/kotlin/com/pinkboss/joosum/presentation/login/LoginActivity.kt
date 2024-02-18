221a566c4d9bb8ea08ab52dd1756211d014ac7c4
최현정 <hjchoi.dev@gmail.com>
2024-02-18 14:44:21 +0900

    Feat : Go to HomeActivity

--- app/src/main/kotlin/com/pinkboss/joosum/presentation/login/LoginActivity.kt
+++ app/src/main/kotlin/com/pinkboss/joosum/presentation/login/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.pinkboss.joosum.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.pinkboss.joosum.R
+import com.pinkboss.joosum.databinding.ActivityLoginBinding
+import com.pinkboss.joosum.presentation.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnLoginGoogle.setOnClickListener { goToHome() }
+            btnLoginKakao.setOnClickListener { goToHome() }
+            btnLoginNaver.setOnClickListener { goToHome() }
+        }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
