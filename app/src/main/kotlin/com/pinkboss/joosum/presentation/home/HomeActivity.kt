11ab18ec89f27d063e9001a6eda27be269aace0b
최현정 <hjchoi.dev@gmail.com>
2024-02-18 15:34:23 +0900

    Feat : Update HomeActivity

--- app/src/main/kotlin/com/pinkboss/joosum/presentation/home/HomeActivity.kt
+++ app/src/main/kotlin/com/pinkboss/joosum/presentation/home/HomeActivity.kt
@@ -3,10 +3,43 @@ package com.pinkboss.joosum.presentation.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pinkboss.joosum.R
+import com.pinkboss.joosum.databinding.ActivityHomeBinding
+import com.pinkboss.joosum.presentation.home.folder.FolderFragment
+import com.pinkboss.joosum.presentation.home.info.InfoFragment
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container_home, HomeFragment())
+                    transaction.commit()
+                    true
+                }
+
+                R.id.navigation_folder -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container_home, FolderFragment())
+                    transaction.commit()
+                    true
+                }
+
+                R.id.navigation_info -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container_home, InfoFragment())
+                    transaction.commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/kotlin/com/pinkboss/joosum/presentation/home/HomeFragment.kt
+++ app/src/main/kotlin/com/pinkboss/joosum/presentation/home/HomeFragment.kt
@@ -1,4 +1,7 @@
 package com.pinkboss.joosum.presentation.home
 
-class HomeFragment {
+import androidx.fragment.app.Fragment
+import com.pinkboss.joosum.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
 }
